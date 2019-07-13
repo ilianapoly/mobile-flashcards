6c5fd3b6275d1545e801c7995730e5c179c8468d
Iliana Polydorou <48098243+ilianapoly@users.noreply.github.com>
2019-07-13 07:41:53 +0300

    added react-redux and redux dependencies

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from 'react-native';
+import reducer from "./reducers";
+
+const store = createStore(reducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <Text>Test</Text>+
+          <AppContainer />
+        </View>
+      </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
